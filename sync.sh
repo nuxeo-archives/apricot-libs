501d242d42ca8df26d360e1ab93c1f4175d42a59
Bogdan Stefanescu <bstefanescu@nuxeo.com>
2011-02-09 23:53:24 +0100

    NXP-6273 - working on osgi

--- sync.sh
+++ sync.sh
@@ -1,17 +1,20 @@
 #!/bin/sh
 
-GEN_DIR=tools/nuxeo-project-generator
-JAR=${GEN_DIR}/target/nuxeo-project-gen-1.0.jar
+GEN_NAME=org.nuxeo.osgi.ide.project.generator
+GEN_DIR=tools/${GEN_NAME}
+JAR=${GEN_DIR}/target/${GEN_NAME}-*.jar
+
+#JAVA_OPTS="$JAVA_OPTS -Xdebug -Xrunjdwp:transport=dt_socket,address=8787,server=y,suspend=y"
 
 if [ ! -f $JAR ]; then
     echo "Building project generator";
     pushd $GEN_DIR;
-    mvn install;
+    mvn3 install;
     popd;
 fi
 
 echo "Synchornizing manifests ..."
 
-java -cp ${JAR} org.nuxeo.build.osgi.gen.SyncManifest "../../" "plugins/pom.xml" "plugins"
+java $JAVA_OPTS -cp ${JAR} org.nuxeo.build.osgi.gen.SyncManifest "../" "plugins/pom.xml" "plugins"
 
 echo "Done."
