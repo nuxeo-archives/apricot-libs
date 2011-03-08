317e30e43a1243158c2d9b30e506f158b4803008
Bogdan Stefanescu <bstefanescu@nuxeo.com>
2011-03-08 22:43:32 +0100

    NXP-6273 - fixed gen.sh

--- build/gen.sh
+++ build/gen.sh
@@ -1,13 +1,13 @@
 #!/bin/bash -i
 
 GEN_NAME=org.nuxeo.osgi.ide.project.generator
-GEN_DIR=ide/project-generator
+GEN_DIR=../ide/project-generator
 JAR=${GEN_DIR}/target/${GEN_NAME}-*.jar
 
 #JAVA_OPTS="$JAVA_OPTS -Xdebug -Xrunjdwp:transport=dt_socket,address=8787,server=y,suspend=y"
 
 echo "Generating plugins ..."
 
-java $JAVA_OPTS -cp ${JAR} org.nuxeo.osgi.ide.project.generator.ProjectGenerator -clean ../ . plugins/pom.xml
+java $JAVA_OPTS -cp ${JAR} org.nuxeo.osgi.ide.project.generator.ProjectGenerator -clean ../../ ../ ../plugins/pom.xml
 
 echo "Done."
