28b49441c1081c3404a50913ae9d619757d756e2
Bogdan Stefanescu <bstefanescu@nuxeo.com>
2011-03-12 12:44:36 +0100

    updated sync.sh

--- build/sync.sh
+++ build/sync.sh
@@ -4,7 +4,6 @@ GEN_NAME=org.nuxeo.osgi.ide.project.generator
 GEN_DIR=../ide/project-generator                                                                                                                               
 JAR=${GEN_DIR}/target/${GEN_NAME}-*.jar                                                                                                                     
 
-echo $JAR
 
 #JAVA_OPTS="$JAVA_OPTS -Xdebug -Xrunjdwp:transport=dt_socket,address=8787,server=y,suspend=y"
 
